4b1b3261c685065e64057dc2a4e3f9f403e0416d
Suzuka Fukami <suzuka.fukami@societyone.com.au>
2021-09-19 18:58:03 +1000

    Wired up pages to navigate through

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -3,7 +3,10 @@
     FILTER
   </section>
   <section>
+    <div class="controls">
+      <button>Refresh</button>
+      <router-link to="/register">Register as Coach</router-link>
+    </div>
     LIST OF COACHES
   </section>
 </template>
-
