e336f8177d05b7d49fc9c4bbc2933838f9a8bb2b
Suzuka Fukami <suzuka.fukami@societyone.com.au>
2021-09-19 21:17:02 +1000

    add static coaches info and display them

--- src/router.js
+++ src/router.js
@@ -7,7 +7,7 @@ import ContactCoach from '@/pages/requests/ContactCoach.vue'
 import RequestsReceived from '@/pages/requests/RequestsReceived.vue'
 import NotFound from '@/pages/NotFound.vue'
 
-const router = createRouter({
+export const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: '/', redirect: '/coaches' },
@@ -23,4 +23,3 @@ const router = createRouter({
   ]
 });
 
-export default router;
