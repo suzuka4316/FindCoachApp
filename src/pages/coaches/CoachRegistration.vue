779617b2875ce2699a878f155d358312c654ab6e
Suzuka Fukami <suzuka.fukami@societyone.com.au>
2021-10-02 15:01:01 +1000

    Update vuex to register new coach

--- src/pages/coaches/CoachRegistration.vue
+++ src/pages/coaches/CoachRegistration.vue
@@ -2,7 +2,7 @@
   <section>
     <base-card>
       <h2>Register as a coach now!</h2>
-      <coach-form></coach-form>
+      <coach-form @save-data="saveData"></coach-form>
     </base-card>
   </section>
 </template>
@@ -12,6 +12,13 @@ import CoachForm from '@/components/coaches/CoachForm.vue';
 export default {
   components: {
     CoachForm
-  }
+  },
+  methods: {
+    saveData(data) {
+      this.$store.dispatch('coaches/registerCoach', data)
+      // by using 'replace' instead of 'push', we forbid user to go back to registration page
+      this.$router.replace('/coaches')
+    }
+  },
 };
 </script>
