c57fdd28286feed74c5f4156ebafbcbd5001e964
Suzuka Fukami <suzuka.fukami@societyone.com.au>
2021-10-02 18:03:11 +1000

    Display fetched coaches

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -24,5 +24,25 @@ export default {
       ...coachData,
       id: userId
     })
+  },
+  async loadCoaches(context) {
+    const response = await fetch(`https://vue-http-demo-bc6fe-default-rtdb.firebaseio.com/coaches.json`)
+    const responseData = await response.json()
+    if (!response.ok) {
+      // error...
+    }
+    const coaches = []
+    for (const key in responseData) {
+      const coach = {
+        id: key,
+        firstName: responseData[key].firstName,
+        lastName: responseData[key].lastName,
+        description: responseData[key].description,
+        hourlyRate: responseData[key].hourlyRate,
+        areas: responseData[key].areas,
+      }
+      coaches.push(coach)
+    }
+    context.commit('setCoaches', coaches)
   }
 }
