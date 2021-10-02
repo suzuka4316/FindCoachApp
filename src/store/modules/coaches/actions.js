9ff6979eb9d20043163c204ae117bdea985b3147
Suzuka Fukami <suzuka.fukami@societyone.com.au>
2021-10-02 17:46:40 +1000

    Store coaches by sending PUT request

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -1,13 +1,28 @@
 export default {
-  registerCoach(context, data) {
+  async registerCoach(context, data) {
+    const userId = context.rootGetters.userId
     const coachData = {
-      id: context.rootGetters.userId,
       firstName: data.first,
       lastName: data.last,
       description: data.desc,
       hourlyRate: data.rate,
       areas: data.areas
     }
-    context.commit('registerCoach', coachData)
+
+    const response = await fetch(`https://vue-http-demo-bc6fe-default-rtdb.firebaseio.com/coaches/${userId}.json`, {
+      method: 'PUT', //update if user already exists in backend
+      body: JSON.stringify(coachData)
+    })
+
+    // const responseData = await response.json()
+
+    if (!response.ok) {
+      // error...
+    }
+
+    context.commit('registerCoach', {
+      ...coachData,
+      id: userId
+    })
   }
 }
