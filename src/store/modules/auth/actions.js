f6375db0287333314ea9eee022a27abf14388322
Suzuka Fukami <suzuka.fukami@societyone.com.au>
2021-10-02 23:54:16 +1000

    Send POST request to create a user

--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -1,1 +1,28 @@
-export default {}
+export default {
+  login() {
+
+  },
+  // https://firebase.google.com/docs/reference/rest/auth#section-create-email-password
+  async signup(context, payload) {
+    const response = await fetch(`https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=
+    AIzaSyBsSxptF79dnDk0b0NLRCrm3wMivLAQ6uk`, {
+      method: 'POST',
+      body: JSON.stringify({
+        email: payload.email,
+        password: payload.password,
+        returnSecureToken: true,
+      })
+    })
+    const responseData = await response.json()
+    if (!response.ok) {
+      const error = new Error(responseData.message || 'Failed to authenticate.')
+      throw error
+    }
+    console.log(responseData)
+    context.commit('setUser', {
+      token: responseData.idToken,
+      userId: responseData.localId,
+      tokenExpiration: responseData.expiresIn,
+    })
+  }
+}
