cc1a9915dca6ffd44e01d0b464c99b9d50f82600
Suzuka Fukami <suzuka.fukami@societyone.com.au>
2021-10-02 18:41:09 +1000

    Add http error handling when fetching coaches

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -8,18 +8,13 @@ export default {
       hourlyRate: data.rate,
       areas: data.areas
     }
-
     const response = await fetch(`https://vue-http-demo-bc6fe-default-rtdb.firebaseio.com/coaches/${userId}.json`, {
       method: 'PUT', //update if user already exists in backend
       body: JSON.stringify(coachData)
     })
-
-    // const responseData = await response.json()
-
     if (!response.ok) {
       // error...
     }
-
     context.commit('registerCoach', {
       ...coachData,
       id: userId
@@ -29,7 +24,8 @@ export default {
     const response = await fetch(`https://vue-http-demo-bc6fe-default-rtdb.firebaseio.com/coaches.json`)
     const responseData = await response.json()
     if (!response.ok) {
-      // error...
+      const error = new Error(responseData.message || "Failed to fetch!")
+      throw error
     }
     const coaches = []
     for (const key in responseData) {
